1d3215de48f2d2ad1971be7c42934cc44d2d8e81
mudassar <khanqureshi31181@gmail.com>
2022-08-29 16:01:24 -0500

    alien

--- index.js
+++ index.js
@@ -20,11 +20,30 @@ function ship(hp, speed) {
   this.fireBullet = function () {
     return new bullet(1, 1);
   };
-}
-
+};
 
 // A Ship has two attributes:
 // HP (Health Points)
 // Speed (how fast it moves)
 // A Ship has one method:
 // fireBullet() returns a Bullet object with 1 speed and 1 damage
+
+function alien(hp, coordinates, speed) {
+  this.hp = hp;
+  this.coordinates = { "x": coordinates.x, "y": coordinates.y }
+  this.speed = { "x": speed.x, "y": speed.y }
+  this.move = function () {
+    this.coordinates.x = this.speed.x + this.coordinates.x
+    console.log(this.coordinates.x)
+
+  }
+
+}
+
+const ali= new alien(10,{"x":5, "y":14},{"x":7,"y":6});
+console.log(ali.move());
+
+
+
+//method move will change x and y cordinates by aliens speed
+// on hit(bullet) method takes a bullet as a para and decrease the aliens hp by the bullet damage
