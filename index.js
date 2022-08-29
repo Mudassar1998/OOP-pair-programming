2328cf978f3e98552097c1dbfa40e94eeb368747
mudassar <khanqureshi31181@gmail.com>
2022-08-29 14:01:37 -0500

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+function bullet(speed , damage) {
+  return{
+    speed,
+    damage,
+  }
+};
+function bullet(speed, damage){
+  this.speed=speed;
+  this.damage=damage;
+};
+class bullet{
+  constructor(speed, damage) {
+  this.speed=speed;
+  this.damage=damage;
+}
+};
