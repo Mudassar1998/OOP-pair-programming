01ed9d00e22ee192e93b0759b6484632b9bb7c52
Elias Grey <eliasgrey9@gmail.com>
2022-08-29 12:51:32 -0700

    fire-bullet

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-function bullet(speed , damage) {
-  return{
-    speed,
-    damage,
-  }
-};
-function bullet(speed, damage){
-  this.speed=speed;
-  this.damage=damage;
-};
-class bullet{
-  constructor(speed, damage) {
-  this.speed=speed;
-  this.damage=damage;
+// function bullet(speed , damage) {
+//   return{
+//     speed,
+//     damage,
+//   }
+// };
+function bullet(speed, damage) {
+  this.speed = speed;
+  this.damage = damage;
 }
-};
+// class bullet{
+//   constructor(speed, damage) {
+//   this.speed=speed;
+//   this.damage=damage;
+// }
+// };
+function ship(hp, speed) {
+  this.hp = hp;
+  this.speed = speed;
+  this.fireBullet = function () {
+    return new bullet(1, 1);
+  };
+}
+
+
+// A Ship has two attributes:
+// HP (Health Points)
+// Speed (how fast it moves)
+// A Ship has one method:
+// fireBullet() returns a Bullet object with 1 speed and 1 damage
